7a9ab99bd88af24419383a21180df71cba008482
Simo320v <free.red5ghz@gmail.com>
2023-04-18 11:52:15 +0200

    Vild Ny Update 2.1, til en ny morderne gui.

--- Python s2/s2026 Update Cleaner Design.py
+++ Python s2/s2026 Update Cleaner Design.py
@@ -18,7 +18,7 @@ def read_data(tree):
         count += 1
 
 
-def edit_record(event, tree):
+def edit_record(tree):
     index_selected = tree.focus()
     values = tree.item(index_selected, 'values')
     entry.delete(0, tk.END)
@@ -34,38 +34,26 @@ tree_select = "#773333"
 oddrow = "#ddeedd"
 evenrow = "#cce0cc"
 
-
-data_list = []
-data_list.append(("Japanese Empire", "N@zi Germany", "USA"))
-data_list.append(("N@zi Germany", "Finland", "Soviet Union"))
-data_list.append(("Fascist Italy", "N@zi Germany", "Greece"))
-data_list.append(("Finland", "N@zi Germany", "Soviet Union"))
-data_list.append(("British Empire", "USA", "N@zi Germany"))
-data_list.append(("France", "British Empire", "N@zi Germany"))
-data_list.append(("Vishy France", "N@zi Germany", "British Empire"))
-data_list.append(("USA", "British Empire", "Japanese Empire"))
-data_list.append(("Soviet Union", "USA", "N@zi Germany"))
-data_list.append(("Republic of China", "British Empire", "Japanese Empire"))
-data_list.append(("Polish Republic", "Japanese Empire", "N@zi Germany"))
+data_list = [("Japanese Empire", "N@zi Germany", "USA"), ("N@zi Germany", "Finland", "Soviet Union"), ("Fascist Italy", "N@zi Germany", "Greece"), ("Finland", "N@zi Germany", "Soviet Union"), ("British Empire", "USA", "N@zi Germany"), ("France", "British Empire", "N@zi Germany"), ("Vishy France", "N@zi Germany", "British Empire"), ("USA", "British Empire", "Japanese Empire"), ("Soviet Union", "USA", "N@zi Germany"), ("Republic of China", "British Empire", "Japanese Empire"), ("Polish Republic", "Japanese Empire", "N@zi Germany")]
 
 root = tk.Tk()
-root.title('Random GUI')
+root.title('🚥 S2026 GUI - 🖥 ▁ ▂ ▄ ▅ ▆ ▇ █彡２.１彡█ ▇ ▆ ▅ ▄ ▂ ▁̲')
 root.geometry("700x600")
 
-Ramme = tk.LabelFrame(root, text="Main Area")
+Ramme = tk.LabelFrame(root, text="Main Område ", background="#F5F5F5")
 Ramme.grid(row=0, column=0, ipadx=90, padx=padx, pady=pady, sticky=tk.N)
 
-Ramme2 = tk.LabelFrame(root, text="Countries WW2")
+Ramme2 = tk.LabelFrame(root, text="Lande WW2", background="#F5F5F5")
 Ramme2.grid(row=1, column=0, pady=10, padx=20, sticky=tk.N)
 
-entry = tk.Entry(Ramme, justify="center", width=40)
+entry = tk.Entry(Ramme, justify="center", width=40, highlightthickness=1, highlightbackground="#C5C5C5", border=0, background="#F6F6F6")
 entry.grid(row=1, column=2, padx=padx, pady=pady)
 entry.insert(0, "Tryk på knappen 'Kill' for at fjerne teksten")
 
 label = tk.Label(Ramme, text="✖ Prøv at fjerne teksten")
 label.grid(row=2, column=3, padx=padx, pady=pady)
 
-knap = tk.Button(root, text=" 🥹 Kill 🥹 ", activebackground="red", command=clean)
+knap = tk.Button(root, text=" 🥹 Kill 🥹 ", activebackground="red", command=clean, highlightthickness=5, border=0, background="#E0EBF9", highlightbackground="#C5C5C5")
 knap.grid(row=0, column=1, padx=padx, pady=pady)
 
 style = ttk.Style()
@@ -90,13 +78,12 @@ tree1.heading("col1", text="Countries", anchor=tk.CENTER)
 tree1.heading("col2", text="Allies", anchor=tk.CENTER)
 tree1.heading("col3", text="Enemies", anchor=tk.CENTER)
 
-tree1.bind("<ButtonRelease-1>", lambda event: edit_record(event, tree1))
+tree1.bind("<ButtonRelease-1>", lambda event: edit_record(tree1))
 
 tree1.tag_configure('oddrow', background=oddrow)
 tree1.tag_configure('evenrow', background=evenrow)
 
 read_data(tree1)
-
 
 if __name__ == '__main__':
     root.mainloop()
